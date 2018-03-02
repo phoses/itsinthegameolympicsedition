6fda17e7ae167c402a44b853af58c1ddbb9a803b
EU\lehtpoll <olli-pekka.lehtonen@tieto.com>
2018-03-02 14:48:22 +0200

    style changes

--- src/main/resources/static/App/TournamentSelect.jsx
+++ src/main/resources/static/App/TournamentSelect.jsx
@@ -8,7 +8,7 @@ class TournamentSelect extends Component {
                 <span className="formselect topic">Tournament:</span>
                 <select className="select" value={this.props.tournamentindex} onChange={this.props.changeTournament}>
                     {this.props.emptySelect &&
-                        <option value="">-- Select tournament-- </option>
+                        <option value="">-- Select tournament --</option>
                     }
                     {this.props.data.tournaments.map(( tournament, i ) =>
                         <option key={i} name="tournament" value={i}>{tournament.name}</option>
