d018f088e523b465c3b3f0d6ecc493136e82fe2a
EU\lehtpoll <olli-pekka.lehtonen@tieto.com>
2018-04-17 15:33:21 +0300

    compare fix

--- src/main/java/com/op/itsinthegame/comparator/ScoretableComparator.java
+++ src/main/java/com/op/itsinthegame/comparator/ScoretableComparator.java
@@ -10,7 +10,15 @@ public class ScoretableComparator implements Comparator<Scoretable>{
 	public int compare(Scoretable o1, Scoretable o2) {
 		
 		if(o1.getAvgpoints().compareTo(o2.getAvgpoints()) == 0){
-			return o1.getWinpros().compareTo(o2.getWinpros()) * -1;
+			if(o1.getWinpros().compareTo(o2.getWinpros()) == 0){
+				if(o1.getAvggoalsfor().compareTo(o2.getAvggoalsfor()) == 0){
+					return o1.getAvggoalsagainst().compareTo(o2.getAvggoalsagainst());
+				}else{
+					return o1.getAvggoalsfor().compareTo(o2.getAvggoalsfor());
+				}
+			}else{
+				return o1.getWinpros().compareTo(o2.getWinpros()) * -1;				
+			}
 		}
 		
 		return o1.getAvgpoints().compareTo(o2.getAvgpoints()) * -1;
